ac6e7490594da1c7a9e77640dfe101890e1f22e4
Suparna Das <105983242+SDisback@users.noreply.github.com>
2022-09-24 13:38:00 +0530

    Create Even_Odd_Kotlin.kt

---
+++ Even_Odd_Kotlin.kt
@@ -0,0 +1,19 @@
+// Coded by Suparna Das.
+
+// Program to find whether number is even or odd in KOTLIN.
+
+// Thanks for watching.
+
+
+fun main(args: Array <String>) 
+{
+    var number = readLine()!!.toInt() 
+    
+    if (number%2==0){
+       println("Number is even")
+    }
+    else{
+        println("Number is odd")
+    }
+        
+}
